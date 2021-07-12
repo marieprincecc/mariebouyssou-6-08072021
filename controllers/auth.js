39f8ecf10d4147a6b76e6732b887bcae1e4a563e
Marie Bouyssou <reveolution.de.saveur@gmail.com>
2021-07-12 13:51:40 +0200

    maj login

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,14 +2,14 @@ const bcrypt = require('bcrypt');
 
 const user = require('../models/utilisateur')    //importation modele user
 
-exports.createUser = (req, res, next) => {   //enregistrement nouvel utilisateur dans la base de données
+exports.createUser = (req, res, next) => {   //creation nouvel utilisateur
   bcrypt.hash(req.body.password, 10)        //hashage du mot de passe
       .then(hash => {
-    const User = new user({
+    const User = new user({                 // creation new user avec hash du mdp
       email: req.body.email,
       password: hash
     });
-    User.save()
+    User.save()                           //enregistrement nouvel utilisateur dans la base de données
       .then(() => res.status(201).json({ message: 'Nouvel utilisateur enregistré !'}))
       .catch(error => res.status(400).json({ error }));
   })
@@ -17,7 +17,22 @@ exports.createUser = (req, res, next) => {   //enregistrement nouvel utilisateur
   };
 
 exports.logingUser = (req, res, next) => {   //authentification utilisateur !!!! A COMPLETER
-    const User = {...req.body}
-      .then(User => res.status(200).json({userId},{token}))
-      .catch(error => res.status(400).json({ error }));
+      user.findOne({ email: req.body.email })   // on cherche l'utilisateur correspondant a l'email de la requet
+      .then(User => {
+        if (!User) {      //si non trouvé
+          return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+        }
+        bcrypt.compare(req.body.password, User.password)    //si trouvé on compare le password avec bcrypt
+          .then(valid => {
+            if (!valid) {         //si mdp incorrect
+              return res.status(401).json({ error: 'Mot de passe incorrect !' });
+            }
+            res.status(200).json({      //si mot de passe correct on renvoi la reponse attendu
+              userId: User._id,
+              token: 'TOKEN'
+            });
+          })
+          .catch(error => res.status(500).json({ error }));
+      })
+      .catch(error => res.status(500).json({ error }));
   }  
