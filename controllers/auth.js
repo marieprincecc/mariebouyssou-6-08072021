0527c230ddfc34005780796e01be8469ea7f961f
Marie Bouyssou <reveolution.de.saveur@gmail.com>
2021-07-12 12:55:58 +0200

    ajout bcrypt et mise a jour createUser

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,19 @@
+const bcrypt = require('bcrypt');
+
 const user = require('../models/utilisateur')    //importation modele user
 
 exports.createUser = (req, res, next) => {   //enregistrement nouvel utilisateur dans la base de données
+  bcrypt.hash(req.body.password, 10)        //hashage du mot de passe
+      .then(hash => {
     const User = new user({
-      ...req.body
+      email: req.body.email,
+      password: hash
     });
     User.save()
       .then(() => res.status(201).json({ message: 'Nouvel utilisateur enregistré !'}))
       .catch(error => res.status(400).json({ error }));
+  })
+      .catch(error => res.status(500).json({ error }));
   };
 
 exports.logingUser = (req, res, next) => {   //authentification utilisateur !!!! A COMPLETER
