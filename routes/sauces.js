0abed0a398391fdd0705ea3ec037b9112fcbd238
Marie Bouyssou <reveolution.de.saveur@gmail.com>
2021-08-04 12:59:23 +0200

    rectification routes

--- routes/sauces.js
+++ routes/sauces.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const authentification = require('../middleware/auth');
 const saucesCtrl = require('../controllers/sauces');
 const multer = require('../middleware/multer-config');
-const verificationUser = require('../middleware/userId');
+
 
 
 router.get('/',authentification, saucesCtrl.getAllSauces);
 router.get('/:id',authentification, saucesCtrl.getOneSauce);
 router.post('/',authentification, multer, saucesCtrl.createSauce);
-router.put('/:id',authentification, verificationUser, multer, saucesCtrl.modifySauce);
-router.delete('/:id',authentification, verificationUser, saucesCtrl.deleteSauce);
+router.put('/:id',authentification, multer, saucesCtrl.modifySauce);
+router.delete('/:id',authentification, saucesCtrl.deleteSauce);
 router.post('/:id/like', authentification, saucesCtrl.LikeDislike);
 
 module.exports = router;
